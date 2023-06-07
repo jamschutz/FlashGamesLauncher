e4aca72fb5b3264ce39c8ed09aa6bf31d8c801fb
Joey Schutz <schutzjoey@gmail.com>
2023-06-07 14:58:47 -0400

    it works with iframes

--- code/loadgames.js
+++ code/loadgames.js
@@ -3,6 +3,7 @@ window.addEventListener("load", (event) => {
     const ruffle = window.RufflePlayer.newest();
     const player = ruffle.createPlayer();
     const container = document.getElementById("container");
+    var game = window.location.hash.substring(1); // lop off the leading '#'
     container.appendChild(player);
-    player.load("games/politics/EmoGame.swf");
+    player.load(`games/politics/${game}.swf`);
 });
