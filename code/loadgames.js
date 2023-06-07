8da9f26aa974cd4dc033ab25471d740537990cc6
Joey Schutz <schutzjoey@gmail.com>
2023-06-07 16:00:55 -0400

    built out lookup

--- code/loadgames.js
+++ code/loadgames.js
@@ -1,9 +1,47 @@
 const gamesLookup = {
+    // politics
     'SpecOps': 'politics/40095_wot202c',
     'EmoGame': 'politics/EmoGame',
     'AntiWarGame': 'politics/antiwar_game/_container',
+    
+    // clones
     'UltimateFlashSonic': 'clones/151706_ultimate_sonic202c',
-    'FlashPortal': 'clones/portal2'
+    'FlashPortal': 'clones/portal2',
+    
+    // cannon
+    'NanacaCrash': 'cannon/',
+    'DropKickTheFaint': 'cannon/',
+    'KittenCannon': 'cannon/',
+    
+    // edgelord
+    'SkullKid': 'edgelord/',
+    'thegame': 'edgelord/',
+    'DivineIntervention': 'edgelord/',
+    
+    // puzzle
+    'IWishIWereTheMoon': 'puzzle/',
+    'Samorost': 'puzzle/',
+    'GrowCube': 'puzzle/',
+    'Cursor10': 'puzzle/',
+
+    // animation
+    'XiaoXiao2': 'animation/',
+    'XiaoXiao4': 'animation/',
+    'FlyGuy': 'animation/',
+    'DungeonEscape': 'animation/',
+
+    // orisinal
+    'TheLittePigs': 'orisinal/',
+    'WinterBells': 'orisinal/',
+    'PocketfulOfStars': 'orisinal/',
+
+    // gamer
+    'BallRevamped2': 'gamer/',
+    'Motherload': 'gamer/',
+    'LittleRocketman': 'gamer/',
+
+    // dressup
+    'CreateARide': 'dressup/'
 }
 
 
