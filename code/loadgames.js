cd94bfcb788cad7d2cd0f05601f63f44f0757d3f
Joey Schutz <schutzjoey@gmail.com>
2023-06-07 15:38:48 -0400

    added in clone games

--- code/loadgames.js
+++ code/loadgames.js
@@ -2,7 +2,8 @@ const gamesLookup = {
     'SpecOps': 'politics/40095_wot202c',
     'EmoGame': 'politics/EmoGame',
     'AntiWarGame': 'politics/antiwar_game/_container',
-    'UltimateFlashSonic': 'clones/151706_ultimate_sonic202c.swf'
+    'UltimateFlashSonic': 'clones/151706_ultimate_sonic202c',
+    'FlashPortal': 'clones/portal2'
 }
 
 
@@ -14,6 +15,10 @@ window.addEventListener("load", (event) => {
     var game = window.location.hash.substring(1); // lop off the leading '#'
     container.appendChild(player);
 
+    console.log(`loading game ${game}`)
+
     var pathToGame = gamesLookup[game];
+
+    console.log(`path to game: ${pathToGame}`)
     player.load(`games/${pathToGame}.swf`);
 });
